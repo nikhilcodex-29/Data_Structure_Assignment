ae937ed7560a58b36621f309da2d027527b4669a
nikhilcodex-29 <nikhilpadarthi20@gmail.com>
2026-04-06 22:21:16 +0530

    Update Q5_circular_queue.c

--- Q5_circular_queue.c
+++ Q5_circular_queue.c
@@ -6,93 +6,93 @@ int queue[MAX];
 int front = -1, rear = -1;
 
 // Enqueue
-void enqueue(int x) {
-    if((rear + 1) % MAX == front) {
-        printf("Queue Overflow\n");
-    } else {
-        if(front == -1)
-            front = 0;
-        rear = (rear + 1) % MAX;
-        queue[rear] = x;
-        printf("Inserted %d\n", x);
-    }
-}
+// void enqueue(int x) {
+//     if((rear + 1) % MAX == front) {
+//         printf("Queue Overflow\n");
+//     } else {
+//         if(front == -1)
+//             front = 0;
+//         rear = (rear + 1) % MAX;
+//         queue[rear] = x;
+//         printf("Inserted %d\n", x);
+//     }
+// }
 
-// Dequeue
-void dequeue() {
-    if(front == -1) {
-        printf("Queue Underflow\n");
-    } else {
-        printf("Deleted %d\n", queue[front]);
-        if(front == rear) {
-            front = rear = -1;
-        } else {
-            front = (front + 1) % MAX;
-        }
-    }
-}
+// // Dequeue
+// void dequeue() {
+//     if(front == -1) {
+//         printf("Queue Underflow\n");
+//     } else {
+//         printf("Deleted %d\n", queue[front]);
+//         if(front == rear) {
+//             front = rear = -1;
+//         } else {
+//             front = (front + 1) % MAX;
+//         }
+//     }
+// }
 
-// Peek
-void peek() {
-    if(front == -1) {
-        printf("Queue is empty\n");
-    } else {
-        printf("Front element: %d\n", queue[front]);
+// // Peek
+// void peek() {
+//     if(front == -1) {
+//         printf("Queue is empty\n");
+//     } else {
+//         printf("Front element: %d\n", queue[front]);
     }
 }
 
 // Display
-void display() {
-    int i;
-    if(front == -1) {
-        printf("Queue is empty\n");
-    } else {
-        printf("Queue elements: ");
-        i = front;
-        while(1) {
-            printf("%d ", queue[i]);
-            if(i == rear)
-                break;
-            i = (i + 1) % MAX;
-        }
-        printf("\n");
+// void display() {
+//     int i;
+//     if(front == -1) {
+//         printf("Queue is empty\n");
+//     } else {
+//         printf("Queue elements: ");
+//         i = front;
+//         while(1) {
+//             printf("%d ", queue[i]);
+//             if(i == rear)
+//                 break;
+//             i = (i + 1) % MAX;
+//         }
+//         printf("\n");
     }
 }
 
-int main() {
-    int choice, x;
+// int main() {
+//     int choice, x;
 
-    while(1) {
-        printf("\n1.Enqueue\n2.Dequeue\n3.Peek\n4.Display\n5.Exit\n");
-        printf("Enter choice: ");
-        scanf("%d", &choice);
+//     while(1) {
+//         printf("\n1.Enqueue\n2.Dequeue\n3.Peek\n4.Display\n5.Exit\n");
+//         printf("Enter choice: ");
+//         scanf("%d", &choice);
 
-        switch(choice) {
-            case 1:
-                printf("Enter value: ");
-                scanf("%d", &x);
-                enqueue(x);
-                break;
+//         switch(choice) {
+//             case 1:
+//                 printf("Enter value: ");
+//                 scanf("%d", &x);
+//                 enqueue(x);
+//                 break;
 
-            case 2:
-                dequeue();
-                break;
+//             case 2:
+//                 dequeue();
+//                 break;
 
-            case 3:
-                peek();
-                break;
+//             case 3:
+//                 peek();
+//                 break;
 
-            case 4:
-                display();
-                break;
+//             case 4:
+//                 display();
+//                 break;
 
-            case 5:
-                return 0;
+//             case 5:
+//                 return 0;
 
-            default:
-                printf("Invalid choice\n");
-        }
-    }
+//             default:
+//                 printf("Invalid choice\n");
+//         }
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
